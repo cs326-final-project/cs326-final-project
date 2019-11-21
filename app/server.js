59b685e63e96d4acf3cbf6dea463ecf03b17b6dc
Matthew Oslan <derpthemeus@gmail.com>
2019-11-21 15:39:31 -0500

    Remove server-side templating

--- app/server.js
+++ app/server.js
@@ -1,11 +1,7 @@
-const fs = require("fs");
 const express = require("express");
 const bodyParser = require("body-parser");
 const User = require("./models/user");
 const mongoose = require("mongoose");
-const handlebars = require("handlebars");
-const expressHandlebars = require("express-handlebars");
-const layouts = require("handlebars-layouts");
 const redditScraper = require("./scrapers/redditScraper");
 
 //url param indicates machine mongodb is running on /nameOfDatabase
@@ -22,16 +18,6 @@ router.use(bodyParser.urlencoded({
 }));
 router.use("/api", require("./api/users"));
 app.use(router);
-
-// Setup Handlebars and addons.
-layouts.register(handlebars);
-const viewsPath = __dirname + "/views";
-app.engine("handlebars", expressHandlebars({
-    defaultLayout: null
-}));
-app.set("view engine", "handlebars");
-app.set("views", viewsPath);
-handlebars.registerPartial("layout", fs.readFileSync(viewsPath + "/layout.handlebars", "utf8"));
 
 app.get("/analyzeData", async(req, res) => {
     console.log(`Received a request to analyze a user's data using Reddit authorization code "${req.query.redditCode}"`);
