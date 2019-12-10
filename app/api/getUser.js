3e548be36c8899c1f38bd449584c1858b1007753
tcharlan <43740047+tcharlan@users.noreply.github.com>
2019-12-10 12:26:50 -0500

    Test.

--- app/api/getUser.js
+++ app/api/getUser.js
@@ -29,4 +29,40 @@ function getUser(req) {
     });
 }
 
+function showUser(cookie) {
+    return new Promise((resolve, reject) => {
+        if (!cookie) {
+            resolve(null);
+        }
+
+        const token = cookie;
+        try {
+            const decoded = jwt.decode(token, SECRET);
+
+            User.findOne({ username: decoded.username }, (err, user) => {
+                if (err) reject(err);
+
+                if (!user) {
+                    reject();
+                } else {
+                    resolve(user);
+                }
+            });
+        } catch (ex) {
+            reject(ex);
+        }
+    });
+}
+
+function readCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+}
+
 module.exports = getUser;
